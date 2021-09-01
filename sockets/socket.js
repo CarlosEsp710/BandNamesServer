d886b7523dcbd5bd5093a968f16a8461901d832a
Carlos Espejel <carlosespejel71@gamil.com>
2021-09-01 18:17:25 -0500

    Lógica para bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,38 @@
 const { io } = require("../index");
 
+const Bands = require("../models/bands");
+const Band = require("../models/band");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Interpol"));
+bands.addBand(new Band("Audioslave"));
+bands.addBand(new Band("Pearl Jam"));
+bands.addBand(new Band("Bon Jovi"));
+bands.addBand(new Band("The Killers"));
+
 // Sockets messages
 io.on("connection", (client) => {
   console.log("Cliente conectado");
+
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
   });
 
-  client.on("message", (payload) => {
-    console.log("Mensaje", payload);
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
 
-    io.emit("message", { admin: "Nuevo mensaje" });
+  client.on("add-band", (payload) => {
+    bands.addBand(new Band(payload.name));
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
   });
 });
